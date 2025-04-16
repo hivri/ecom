fea3e1e5bf842935ea5ef1aa150c39c575b19ef3
hitesh v basri <hitesh.basri@gmail.com>
2025-04-17 00:54:23 +0530

    deleted dockerfile,reverted changes

--- includes/db.php
+++ includes/db.php
@@ -1,18 +1,3 @@
 <?php
-
-// Connection string for PostgreSQL
-$conn_string = "host=dpg-cvvbdjje5dus73bbcmh0-a 
-                port=5432 
-                dbname=estore_78rg 
-                user=estore_78rg_user 
-                password=DbGZwf9Fu0gw6E4eECc35IyPEagYF97L";
-
-// Connect to PostgreSQL
-$con = pg_connect($conn_string);
-
-// Check connection
-if (!$con) {
-    die("Connection failed: " . pg_last_error());
-}
-
+$con = mysqli_connect("localhost","root","","Estore");
 ?>
