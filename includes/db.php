6d0964bec0875dfd4e7ba0cfec39ee8ac960448d
hitesh v basri <hitesh.basri@gmail.com>
2025-05-06 00:56:52 +0530

    db changes

--- includes/db.php
+++ includes/db.php
@@ -1,3 +1,19 @@
+/* <?php */
+/* $con = mysqli_connect("localhost","root","","Estore"); */
+/* ?> */
+
+
 <?php
-$con = mysqli_connect("localhost","root","","Estore");
+$con = mysqli_connect(
+    "sql12.freesqldatabase.com", // host
+    "sql12777116",               // username
+    "h6zXiiRbLB",                // password
+    "sql12777116",               // database name
+    3306                         // port
+);
+
+// Check connection
+if (mysqli_connect_errno()) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
